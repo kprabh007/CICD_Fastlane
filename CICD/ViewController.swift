6b9be5133d3e88c6a386d97e8b718265d3aa1c94
kprabhu <kprabhu@deloitte.com>
2020-02-19 11:52:59 +0530

    Initial Commit

---
+++ CICD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CICD
+//
+//  Created by Prabhu, Kaustubh on 19/02/20.
+//  Copyright © 2020 Prabhu, Kaustubh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
